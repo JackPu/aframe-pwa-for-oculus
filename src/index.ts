5f4989cf09d49d468800dfa50c6d2f3c9b5a274c
JackPu <kakashjack@gmail.com>
2022-10-29 15:08:10 +0800

    mod: 替换全景视频素材

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+// import 'aframe';
+
 const videoEl = document.querySelector('a-video');
 
 console.log(videoEl);
@@ -10,7 +12,6 @@ btn?.addEventListener('click', () => {
 
 AFRAME.registerComponent('cursor-listener', {
   init: function () {
-    var lastIndex = -1;
     this.el.addEventListener('click', function (evt) {
       videoEl?.play();
       console.log('I was clicked at: ', evt.detail.intersection.point);
