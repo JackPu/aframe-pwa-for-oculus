20eaf694894602da9b53bb33edc19e922f498084
JackPu <kakashjack@gmail.com>
2022-10-29 17:48:04 +0800

    mod: 更新 PWA 素材

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
 // import 'aframe';
 
-const videoEl = document.querySelector('a-video');
-
-console.log(videoEl);
+const videoEl = document.querySelector('#video') as HTMLVideoElement;
 
 const btn = document.querySelector('#button');
 
 btn?.addEventListener('click', () => {
   // videoEl?.play();
 });
-
+const AFRAME = (window as any).AFRAME;
 AFRAME.registerComponent('cursor-listener', {
   init: function () {
     this.el.addEventListener('click', function (evt) {
+      const videoContainer = document.querySelector('video-container') as any;
+      videoContainer.visible = true;
       videoEl?.play();
       console.log('I was clicked at: ', evt.detail.intersection.point);
     });
